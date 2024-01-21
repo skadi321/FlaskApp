6806226d66ce5e154d52513d8b62ddefad42ea2f
Nikola Grgić <31631877+skadi321@users.noreply.github.com>
2024-01-21 16:41:51 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/greeting', methods=['GET'])
+def get_welcome():
+    return jsonify({"message": "Welcome to the Simple Python API!"})
+
+@app.route('/api/greeting/<name>', methods=['GET'])
+def greet_user(name):
+    greeting = f"Hello, {name}! Welcome to the world of Python APIs."
+    return jsonify({"greeting": greeting})
+
+if __name__ == '__main__':
+    app.run(debug=True)
