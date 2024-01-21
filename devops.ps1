4ffa453849b1c0d8e4331f7446e18c434ed22bb5
Nikola Grgić <31631877+skadi321@users.noreply.github.com>
2024-01-21 23:29:59 +0100

    Add files via upload

---
+++ devops.ps1
@@ -0,0 +1,28 @@
+﻿$listener = New-Object System.Net.Sockets.TcpListener([System.Net.IPAddress]::Any, 12345)
+$listener.Start()
+
+Write-Host "Waiting for incoming connections..."
+
+while ($true) {
+    $client = $listener.AcceptTcpClient()
+    $stream = $client.GetStream()
+    $reader = [System.IO.StreamReader]($stream)
+    $writer = [System.IO.StreamWriter]($stream)
+
+    $message = $reader.ReadLine()
+    Write-Host "Received message: $message"
+
+    if ($message -eq "Build Succeeded!") {
+        git clone "https://github.com/skadi321/FlaskApp.git"
+
+        # Build Docker image
+        cd "FlaskApp"
+        
+         
+        docker build -t simple-python-api .
+        docker run -p 5000:5000 --name simple-python-api -d simple-python-api
+    }
+}
+
+# Close the listener outside the loop
+$listener.Stop()
